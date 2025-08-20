8d3e03616705d3899973458dd00aa40744b6176e
Jose Luis <joseluism1412@gmail.com>
2025-08-20 17:54:22 -0500

    robots and sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 });
