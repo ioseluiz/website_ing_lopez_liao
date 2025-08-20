41f80e68376eaa32ea91b3c71551132d15c63658
Jose Luis <joseluism1412@gmail.com>
2025-08-20 17:10:16 -0500

    load projects photos and fix favicon

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,6 +6,45 @@ import pacificCenterLosaImg from '../assets/pacific_center_losa_bolos.png';
 import cuartoBombasImg from '../assets/cuarto_bombas.png';
 import galeraSevenSeasImg from '../assets/galera_seven_seas.png';
 import helipuertoImg from '../assets/helipuerto.png';
+import galeraPmaViejo from '../assets/galeraPanamaViejoImg.png';
+
+// Importar imágenes de la galería del proyecto Muro Rompeolas
+import muroRompeolas01 from '../assets/proyecto_01_muro_rompeolas/muro_rompeolas_01.png';
+import muroRompeolas02 from '../assets/proyecto_01_muro_rompeolas/muro_rompeolas_02.png';
+import muroRompeolas03 from '../assets/proyecto_01_muro_rompeolas/muro_rompeolas_03.png';
+// Importar imagenes de la galeria del proyecto Cancha Multiuso Scala
+import canchaScala01 from '../assets/proyecto_02_cancha_scala/cancha_scala_01.png';
+import canchaScala02 from '../assets/proyecto_02_cancha_scala/cancha_scala_02.png';
+import canchaScala03 from '../assets/proyecto_02_cancha_scala/cancha_scala_03.png';
+// Import imagenes de la galeria del proyecto Playa Dorada
+import playaDorada01 from '../assets/proyecto_03_playa_dorada/playa_dorada_01.png';
+import playaDorada02 from '../assets/proyecto_03_playa_dorada/playa_dorada_02.png';
+import playaDorada03 from '../assets/proyecto_03_playa_dorada/playa_dorada_03.png';
+// Import imagenes de la galeria del proyecto Escalera Pacific Center
+import escaleraPacific01 from '../assets/proyecto_04_escalera_pacific/escalera_pacific_01.png';
+import escaleraPacific02 from '../assets/proyecto_04_escalera_pacific/escalera_pacific_02.png';
+import escaleraPacific03 from '../assets/proyecto_04_escalera_pacific/escalera_pacific_03.png';
+// Import imagenes de la losa de Jungla Bownling en Pacific Center
+import losaBowling01 from '../assets/proyecto_05_losa_bowling/losa_bowling_01.png';
+import losaBowling02 from '../assets/proyecto_05_losa_bowling/losa_bowling_02.png';
+import losaBowling03 from '../assets/proyecto_05_losa_bowling/losa_bowling_03.png';
+// Import imagenes del cuarto de bombas MELO
+import ctoBomba01 from '../assets/proyecto_06_melo/melo_cto_bombas_01.png';
+import ctoBomba02 from '../assets/proyecto_06_melo/melo_cto_bombas_02.png';
+import ctoBomba03 from '../assets/proyecto_06_melo/melo_cto_bombas_03.png';
+// Import imagenes galera seven seas
+import galSeven01 from '../assets/proyecto_07_galera_seven_seas/galera_seven_01.png';
+import galSeven02 from '../assets/proyecto_07_galera_seven_seas/galera_seven_02.png';
+import galSeven03 from '../assets/proyecto_07_galera_seven_seas/galera_seven_03.png';
+// Import imagenes helipuerto Pacific Center
+import heliPuerto01 from '../assets/proyecto_08_helipuerto/helipuerto_01.png';
+import heliPuerto02 from '../assets/proyecto_08_helipuerto/helipuerto_02.png';
+import heliPuerto03 from '../assets/proyecto_08_helipuerto/helipuerto_03.png';
+// Import imagenes galera 026 Panama Viejo
+import galPmaViejo01 from '../assets/proyecto_09_galera_pma_viejo/galera_PmaViejo_01.png';
+import galPmaViejo02 from '../assets/proyecto_09_galera_pma_viejo/galera_PmaViejo_02.png';
+
+
 
 export const projects = [
   { 
@@ -17,9 +56,9 @@ export const projects = [
     area: "456.00 m²",
     description: "Construcción de muro de retención y rompeolas frente a tablaestaca colapsada para protección costera.",
     gallery: [
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+1",
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+2",
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+3",
+        muroRompeolas01,
+        muroRompeolas02,
+        muroRompeolas03,
     ]
   },
   { 
@@ -31,8 +70,9 @@ export const projects = [
     area: "660.00 m²",
     description: "Construcción de una cancha techada multiuso para la Escuela Scala en Penonomé, incluyendo estructura de acero y cubierta.",
     gallery: [
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+1",
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+2",
+        canchaScala01,
+        canchaScala02,
+        canchaScala03
     ]
   },
   { 
@@ -44,7 +84,9 @@ export const projects = [
     area: "770.00 m²",
     description: "Construcción completa de 9 residencias unifamiliares del modelo 'Arena' en el proyecto Playa Dorada.",
     gallery: [
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+1",
+        playaDorada01,
+        playaDorada02,
+        playaDorada03
     ]
   },
   { 
@@ -56,8 +98,9 @@ export const projects = [
     area: "No especificado",
     description: "Fabricación e instalación de una estructura de acero compleja para una escalera arquitectónica y un puente de conexión dentro de Pacific Center.",
     gallery: [
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+1",
-        "https://placehold.co/800x600/008080/FFFFFF?text=Foto+2",
+        escaleraPacific01,
+        escaleraPacific02,
+        escaleraPacific03
     ]
   },
   { 
@@ -68,7 +111,11 @@ export const projects = [
     img: pacificCenterLosaImg,
     area: "630.00 m²",
     description: "Construcción de una losa estructural de acero diseñada específicamente para soportar las pistas de bolos en Pacific Center.",
-    gallery: []
+    gallery: [
+        losaBowling01,
+        losaBowling02,
+        losaBowling03
+    ]
   },
   { 
     slug: "sistema-incendio-melo",
@@ -78,7 +125,11 @@ export const projects = [
     img: cuartoBombasImg,
     area: "60 m²",
     description: "Construcción del cuarto de bombas, la base del tanque y el montaje del tanque para el sistema contra incendios en la galera de MELO en Cabuya.",
-    gallery: []
+    gallery: [
+        ctoBomba01,
+        ctoBomba03,
+        ctoBomba02
+    ]
   },
   { 
     slug: "techo-galera-seven-seas",
@@ -88,7 +139,11 @@ export const projects = [
     img: galeraSevenSeasImg,
     area: "13,600 m²",
     description: "Reemplazo completo de la cubierta de techo de zinc por panel sándwich para mejorar el aislamiento térmico y la durabilidad en la Zona Libre de Colón.",
-    gallery: []
+    gallery: [
+        galSeven01,
+        galSeven02,
+        galSeven03
+    ]
   },
   { 
     slug: "helipuerto-panama-clinic",
@@ -98,6 +153,24 @@ export const projects = [
     img: helipuertoImg,
     area: "Costo por Administración",
     description: "Diseño, construcción y gestión completa de trámites para la aprobación del permiso de operación del helipuerto en Aeronáutica Civil, que actualmente cuenta con certificación aprobada por la AAC.",
-    gallery: []
+    gallery: [
+        heliPuerto01,
+        heliPuerto02,
+        heliPuerto03
+    ]
+  },
+  {
+    slug: "galera-panama-viejo",
+    name: "Galera 026 de Panamá Viejo Business Center", 
+    client: "Tecpa Group, S.A.", 
+    year: "2018", 
+    img: galeraPmaViejo,
+    area: "456.00 m²",
+    description: "Construcción de muro de fundaciones de galera. Proyecto Entregado",
+    gallery: [
+        galPmaViejo01,
+        galPmaViejo02
+    ]
+
   },
 ];
